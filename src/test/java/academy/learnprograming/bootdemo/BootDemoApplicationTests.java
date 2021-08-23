7e6fa9c8d90c9fd294d9505bf3d1c959a4af050e
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-23 13:26:24 +0900

    Initial commit

---
+++ src/test/java/academy/learnprograming/bootdemo/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.learnprograming.bootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
